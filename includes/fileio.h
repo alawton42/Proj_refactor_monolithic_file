2707683526545a2defdf6b2bf0b9a21839666fc8
Audrey Lawton <alawton42@gmail.com>
2020-09-26 19:41:00 -0400

    Finished this time I think

---
+++ includes/fileio.h
@@ -0,0 +1,11 @@
+
+#ifndef FILEIO_H_
+#define FILEIO_H_
+#include <string>
+#include <vector>
+#include "constants.h"
+
+int loadData(const std::string filename, std::vector<process> &myProcesses);
+int saveData(const std::string filename, std::vector<process> &myProcesses);
+
+#endif
---
+++ includes/utils.h
@@ -0,0 +1,13 @@
+
+#ifndef UTILS_H_
+#define UTILS_H_
+#include <vector>
+#include "constants.h"
+
+
+void sortData(const SORT_ORDER &mySortOrder,std::vector<process> &myProcesses);
+process getNext(std::vector<process> &myProcesses);
+int getSize(std::vector<process> &myProcesses);
+int handleMissingData(std::vector<process> &myProcesses);
+
+#endif
